1a922c981f377d84a886043df0f3a11d0a5a0746
jemoo <wpan1060@naver.com>
2022-05-27 23:03:07 +0900

    test: 포트 8082번호 test

--- src/main/java/com/sparta/post/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/post/security/WebSecurityConfig.java
@@ -37,6 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/api/postsInfo**").permitAll()
                 .antMatchers("/detailPostView**").permitAll()
+                // cicd 포트변경시에 profile 메소드 열어줘야됨
                 .antMatchers("/**").permitAll()
                 .antMatchers("/images/**").permitAll()
                 // css 폴더를 login 없이 허용
